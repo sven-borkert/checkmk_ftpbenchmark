07086eb085a4cb153f2b871df01c17d05c6dde0f
Sven Borkert <sven@borkert.net>
2024-09-02 14:20:22 +0000

    Implementing

--- graphing/ftpbenchmark.py
+++ graphing/ftpbenchmark.py
@@ -5,47 +5,79 @@ from cmk.graphing.v1 import metrics, Title, graphs
 UNIT_NUMBER = metrics.Unit(metrics.DecimalNotation(""), metrics.StrictPrecision(0))
 
 metric_a = metrics.Metric(
-    name="average_download_seconds",
+    name="download_average_duration",
     title=Title("Average seconds per download"),
     unit=metrics.Unit(notation=metrics.DecimalNotation("s")),
     color=metrics.Color.DARK_GREEN
 )
 
 metric_b = metrics.Metric(
-    name="digest_errors",
+    name="download_digest_errors",
     title=Title("Corrupted files after download"),
     unit=UNIT_NUMBER,
     color=metrics.Color.DARK_YELLOW
 )
 
 metric_c = metrics.Metric(
-    name="errors",
-    title=Title("Error responses from server"),
+    name="download_errors",
+    title=Title("Error responses during uploads"),
     unit=UNIT_NUMBER,
     color=metrics.Color.LIGHT_RED
 )
 
 metric_d = metrics.Metric(
-    name="exceptions",
-    title=Title("Exceptions thrown while benchmarking"),
+    name="download_exceptions",
+    title=Title("Exceptions thrown during uploads"),
     unit=UNIT_NUMBER,
     color=metrics.Color.DARK_RED
 )
 
 metric_e = metrics.Metric(
-    name="timeouts",
-    title=Title("Timeouts during file transfers"),
+    name="download_timeouts",
+    title=Title("Timeouts during downloads"),
     unit=UNIT_NUMBER,
     color=metrics.Color.LIGHT_ORANGE
 )
 
-graph_jean_claude_pillemann = graphs.Graph(
-    name="errors_combined",
-    title=Title("Errors during benchmarking"),
+metric_g = metrics.Metric(
+    name="upload_errors",
+    title=Title("Error responses during uploads"),
+    unit=UNIT_NUMBER,
+    color=metrics.Color.LIGHT_RED
+)
+
+metric_h = metrics.Metric(
+    name="upload_exceptions",
+    title=Title("Exceptions thrown during uploads"),
+    unit=UNIT_NUMBER,
+    color=metrics.Color.DARK_RED
+)
+
+metric_i = metrics.Metric(
+    name="upload_timeouts",
+    title=Title("Timeouts during uploads"),
+    unit=UNIT_NUMBER,
+    color=metrics.Color.LIGHT_ORANGE
+)
+
+graph_a = graphs.Graph(
+    name="download_errors_combined",
+    title=Title("Errors during download benchmarking"),
     simple_lines=[
-        "digest_errors",
-        "errors",
-        "exceptions",
-        "timeouts"
+        "download_digest_errors",
+        "download_errors",
+        "download_exceptions",
+        "download_timeouts"
+    ]
+)
+
+graph_b = graphs.Graph(
+    name="upload_errors_combined",
+    title=Title("Errors during upload benchmarking"),
+    simple_lines=[
+        "download_digest_errors",
+        "download_errors",
+        "download_exceptions",
+        "download_timeouts"
     ]
 )
